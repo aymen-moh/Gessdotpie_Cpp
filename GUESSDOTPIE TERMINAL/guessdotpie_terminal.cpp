aaeb451a08ffe813e5a27bb57f379e47ac0dc09e
aymen-moh <aymen.mohammed.abdl@gmail.com>
2026-09-11 08:16:45 +0000

    STarting to add my first game (coin flip)

---
+++ GUESSDOTPIE TERMINAL/guessdotpie_terminal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// i will start my games with the simplest of all: double or nothing (coin flip)
+int flip(int balance, int bet){
+    srand(time(NULL));
+    
+
+
+
+}
+int main(){
+    
+
+}
--- newcomer.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-// i am just making a basic program to test time tracking
-int main(){
-    
-}
