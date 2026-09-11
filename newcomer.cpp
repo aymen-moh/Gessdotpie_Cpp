f5b947460b61fe7abec382daafcfff7fd49cff24
aymen-moh <aymen.mohammed.abdl@gmail.com>
2026-09-11 07:31:58 +0000

    TIme tracking works!!

--- newcomer.cpp
+++ newcomer.cpp
@@ -2,7 +2,9 @@
 // i am just making a basic program to test time tracking
 int main(){
     std::cout << "hello world!\n";
-    std::cout << "what's your name?/n";
+    std::cout << "what's your name?\n";
     std::string name;
     getline(std::cin, name);
+    std::cout << "Hello " << name;
+    
 }
