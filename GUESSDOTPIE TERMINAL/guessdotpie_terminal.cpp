5f95cf090ebef0773e41195e51a080b3b6093cbc
aymen-moh <aymen.mohammed.abdl@gmail.com>
2026-09-11 13:21:54 +0000

    Added the first game but havent tested it out yet!

--- GUESSDOTPIE TERMINAL/guessdotpie_terminal.cpp
+++ GUESSDOTPIE TERMINAL/guessdotpie_terminal.cpp
@@ -3,7 +3,13 @@
 // i will start my games with the simplest of all: double or nothing (coin flip)
 int flip(int balance, int bet){
     srand(time(NULL));
-    
+    int num = rand() % 2;
+    if(num == 1){ // win
+        balance += bet;
+    }
+    else{// loss
+        balance -= bet;
+    }
 
 
 
