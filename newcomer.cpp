9416d1e46b8b9ea4e10270b92aa08f186a88bb01
aymen-moh <aymen.mohammed.abdl@gmail.com>
2026-09-11 07:25:41 +0000

    Second commit.

---
+++ newcomer.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// i am just making a basic program to test time tracking
+int main(){
+    std::cout << "hello world!\n";
+    std::cout << "what's your name?/n";
+    std::string name;
+    getline(std::cin, name);
+}
